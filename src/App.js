d53a42772842634c99e02424b642bfe520aca131
nanchengcyu <1146137024@qq.com>
2024-01-13 00:57:29 +0800

    react 基础study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+function getName() {
+    return 'nanchengyu'
+}
+
+const isLogin = false
+const num = 3 //0 1 3
+//定义核心函数
+function getPicture() {
+    if (num === 0) {
+        return "无图"
+    } else if (num === 1) {
+        return "单图"
+    } else {
+        return "three p"
+    }
+}
+
+const handleClick = (name) => {
+    console.log('click', name)
+}
+
+function Button() {
+    return <button>click me</button>
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            nanchengyu
+            {/*//函数调用*/}
+            {getName()}
+            {/*//方法调用*/}
+            <br></br>
+            {new Date().getDay()}
+            {/*使用js对象*/}
+            <div style={{color: "blue"}}>hhhh</div>
+            <br></br>
+            {isLogin && <span>this is login</span>}
+            {isLogin ? <span>login success</span> : <span>loading</span>}
+            <br></br>
+
+            {getPicture()}
+            {/*// 事件参数e*/}
+
+            return(
+            <div className="App">
+                <button onClick={() => handleClick('ncy')}> click me
+                </button>
+            </div>)
+
+            <button></button>
+            <button/>
+        < /div>
+    )
+        ;
 }
 
 export default App;
