fc66eb60eaf15716693e73a97ac78613064bce4d
nanchengcyu <1146137024@qq.com>
2024-01-13 17:16:32 +0800

    继续study 父子组件

--- src/App.js
+++ src/App.js
@@ -1,58 +1,30 @@
-function getName() {
-    return 'nanchengyu'
-}
+import {useState} from "react";
 
-const isLogin = false
-const num = 3 //0 1 3
-//定义核心函数
-function getPicture() {
-    if (num === 0) {
-        return "无图"
-    } else if (num === 1) {
-        return "单图"
-    } else {
-        return "three p"
+function Detail() {
+    const [status, setStatus] = useState(false)
+
+    function handleClick() {
+     setStatus(!status)
     }
-}
 
-const handleClick = (name) => {
-    console.log('click', name)
-}
-
-function Button() {
-    return <button>click me</button>
-}
-
-function App() {
     return (
-        <div className="App">
-            nanchengyu
-            {/*//函数调用*/}
-            {getName()}
-            {/*//方法调用*/}
-            <br></br>
-            {new Date().getDay()}
-            {/*使用js对象*/}
-            <div style={{color: "blue"}}>hhhh</div>
-            <br></br>
-            {isLogin && <span>this is login</span>}
-            {isLogin ? <span>login success</span> : <span>loading</span>}
-            <br></br>
-
-            {getPicture()}
-            {/*// 事件参数e*/}
-
-            return(
-            <div className="App">
-                <button onClick={() => handleClick('ncy')}> click me
-                </button>
-            </div>)
-
-            <button></button>
-            <button/>
-        < /div>
+        <div>
+            <p style={{
+                display:status ? 'block':'none'
+            }}>
+                Detail的content
+            </p>
+            <button onClick={handleClick}>按钮</button>
+        </div>
     )
-        ;
 }
 
-export default App;
+export default function App() {
+    return (
+        <>
+
+            {/*导入父组件*/}
+            <Detail/>
+        </>
+    )
+}
